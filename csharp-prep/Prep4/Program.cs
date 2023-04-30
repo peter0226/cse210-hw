2a82704c32679feed8f42b259db7c69e6e7edf97
Pedro Pastor <5apeterpastor@gmail.com>
2023-04-30 00:17:38 -0600

    Lists Program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (true) {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            numbers.Add(number);
+        }
+        
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+        
+        Console.WriteLine("The sum is: " + sum);
+        Console.WriteLine("The average is: " + average);
+        Console.WriteLine("The largest number is: " + max);
+        
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        int smallestPositiveNumber = positiveNumbers.Any() ? positiveNumbers.Min() : 0;
+        
+        Console.WriteLine("The smallest positive number is: " + smallestPositiveNumber);
+        
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers) {
+            Console.WriteLine(number);
+        }
     }
 }
